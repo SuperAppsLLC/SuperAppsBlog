43b94e239f5f19a568e37b1da7da645c824977a7
Pratap <psr691995@gmail.com>
2024-01-16 16:03:19 +0530

    database added

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,57 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'postgres');
+
+  const connections = {
+    mysql: {
+      connection: {
+        connectionString: env('strapi-database.cu1kwklhpesh.us-east-1.rds.amazonaws.com'),
+        host: env('DATABASE_HOST', 'strapi-database.cu1kwklhpesh.us-east-1.rds.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi-database'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'Test123'),
+        ssl: { rejectUnauthorized: false },
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      options: {
+          ssl: false
+        },
     },
-    useNullAsDefault: true,
-  },
-});
+    postgres: {
+      connection: {
+        connectionString: env('strapi-database1.cu1kwklhpesh.us-east-1.rds.amazonaws.com'),
+        host: env('DATABASE_HOST', 'strapi-database1.cu1kwklhpesh.us-east-1.rds.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'Test12345'),
+        ssl: { rejectUnauthorized: false },
+        schema: env('DATABASE_SCHEMA', 'public'),
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      options: {
+          ssl: false
+        },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', 'data.db')
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
