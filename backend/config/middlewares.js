40e9570fc5211e78d9e98df35e6db0b380f3c4e8
Pratap <psr691995@gmail.com>
2024-01-17 23:02:25 +0530

    added s3 changes

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,6 +1,34 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+   /* Replace 'strapi::security', with this snippet */
+      /* Beginning of snippet */
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+      /* End of snippet */
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,19 +37,6 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::security',
-    config: {
-        contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-                'connect-src': ["'self'", 'https:'],
-                'img-src': ["'self'", 'data:', 'blob:'],
-                'media-src': ["'self'", 'data:', 'blob:'],
-            },
-        },
-    },
-  },
     {
     name: 'strapi::cors',
     config: {
