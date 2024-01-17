3ae9f050e48c0fba8fab7703901d9d4f973a3ab9
Pratap <psr691995@gmail.com>
2024-01-17 13:10:22 +0530

    added origin

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -9,4 +9,26 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+        contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+                'connect-src': ["'self'", 'https:'],
+                'img-src': ["'self'", 'data:', 'blob:'],
+                'media-src': ["'self'", 'data:', 'blob:'],
+            },
+        },
+    },
+  },
+    {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://blog.superapps.com',"https://admin.superapps.com"],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
 ];
